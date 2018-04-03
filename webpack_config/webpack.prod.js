08f079311f7aa2b16d3a75f357941503f95233c9
james-prado <jamesprado7@gmail.com>
2018-04-03 14:21:55 -0400

    fix build issues

--- webpack_config/webpack.prod.js
+++ webpack_config/webpack.prod.js
@@ -1,41 +1,44 @@
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const common = require('./webpack.common.js');
-const merge = require('webpack-merge');
-const webpack = require('webpack');
-const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const common = require('./webpack.common.js')
+const merge = require('webpack-merge')
+const webpack = require('webpack')
+const path = require('path')
 
 const config = merge(common, {
-  output: {
-    publicPath: './'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$|\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader'
-            },
-            {
-              loader: 'resolve-url-loader'
-            },
-            {
-              loader: 'sass-loader'
-            }
-          ]
-        })
-      }
-    ]
-  },
-  plugins: [
-    // new UglifyJSPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    })
-  ]
-});
+	output: {
+		publicPath: './',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$|\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: [
+						{
+							loader: 'css-loader',
+							options: { minimize: true },
+						},
+						// { loader: 'postcss-loader', options: { sourceMap: true } },
+						{
+							loader: 'resolve-url-loader',
+						},
+						{
+							loader: 'sass-loader',
+							options: { sourceMap: true },
+						},
+					],
+				}),
+			},
+		],
+	},
+	plugins: [
+		new UglifyJSPlugin(),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': 'production',
+		}),
+	],
+})
 
-module.exports = config;
+module.exports = config
