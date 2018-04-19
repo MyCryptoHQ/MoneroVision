a9c49f6130bf793092d05c9a375066457fbb8400
james-prado <jamesprado7@gmail.com>
2018-04-19 08:12:24 -0400

    Add 404 page

--- src/app/page-not-found/index.tsx
+++ src/app/page-not-found/index.tsx
@@ -2,5 +2,12 @@ import * as React from 'react';
 import './page-not-found.scss';
 
 export const PageNotFound = () => {
-  return <div>foo bar</div>;
+  return (
+    <div className="Page-Not-Found card">
+      <h1 className="Page-Not-Found-title">Page not found</h1>
+      <p className="Page-Not-Found-description">
+        The page your looking for couldn't be found. Try the <a href="/">home page</a>
+      </p>
+    </div>
+  );
 };
