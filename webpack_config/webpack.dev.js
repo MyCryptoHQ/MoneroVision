83401c6d6c70445839e6144473ee88740140b13a
james-prado <jamesprado7@gmail.com>
2018-04-27 02:34:54 -0400

    Add webpack bar during development

--- webpack_config/webpack.dev.js
+++ webpack_config/webpack.dev.js
@@ -1,31 +1,33 @@
-const merge = require('webpack-merge')
-const common = require('./webpack.common.js')
-const webpack = require('webpack')
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const WebpackBar = require('webpackbar');
+const webpack = require('webpack');
 
 const config = merge(common, {
-	entry: ['react-hot-loader/patch', common.entry],
-	devtool: 'source-map',
-	devServer: {
-		contentBase: './dist',
-		historyApiFallback: true,
-		hot: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.scss$|\.css$/,
-				use: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader?sourceMap'],
-			},
-		],
-	},
+  entry: ['react-hot-loader/patch', common.entry],
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$|\.css$/,
+        use: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader?sourceMap']
+      }
+    ]
+  },
 
-	plugins: [
-		new webpack.NamedModulesPlugin(),
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify('development'),
-		}),
-	],
-})
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new WebpackBar(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ]
+});
 
-module.exports = config
+module.exports = config;
