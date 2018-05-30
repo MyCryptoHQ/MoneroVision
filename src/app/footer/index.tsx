ffe08eb420780c3e747134b702b133ce9a12a400
james-prado <jamesprado7@gmail.com>
2018-05-30 19:11:42 -0400

    Use flexbox in footer

--- src/app/footer/index.tsx
+++ src/app/footer/index.tsx
@@ -5,18 +5,21 @@ import './footer.scss';
 
 export const Footer = () => (
   <footer className="Footer">
-    <section className="Footer-wrap">
-      <div className="Footer-left">
-        <a target="_blank" rel="noopener noreferrer" href="https://mycrypto.com">
-          <PoweredByMyCrypto />
-        </a>
-      </div>
-
-      <div className="Footer-center">
-        <a href="/">
+    <div className="Footer-inner">
+      <a
+        className="Footer-logo mycrypto"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://mycrypto.com"
+      >
+        <PoweredByMyCrypto />
+      </a>
+      <div className="flex-spacer" />
+      <div className="Footer-main-content">
+        <a className="Footer-logo" href="https://monerovision.com/">
           <Logo />
         </a>
-        <p className="Footer-text">
+        <p className="Footer-main-content-text">
           MoneroVision is an open-source Monero block explorer that is offered as a part of the{' '}
           <a target="_blank" rel="noopener noreferrer" href="https://mycrypto.com">
             MyCrypto
@@ -25,67 +28,71 @@ export const Footer = () => (
           your hands and this is just the beginning of our Monero offerings.
         </p>
       </div>
-
-      <div className="Footer-right">
+      <div className="flex-spacer" />
+      <div className="Footer-social-media-wrapper">
         <p className="Footer-social-titles">MoneroVision</p>
-        <a
-          className="Footer-social-media-link"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://twitter.com/monerovision"
-        >
-          <i className="nc-icon nc-logo-twitter size_24px" />
-        </a>
-        <a
-          className="Footer-social-media-link"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/MyCryptoHQ/MoneroVision"
-        >
-          <i className="nc-icon nc-logo-github size_24px" />
-        </a>
+        <div className="Footer-social-titles-wrapper">
+          <a
+            className="Footer-social-media-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/monerovision"
+          >
+            <i className="nc-icon nc-logo-twitter size_24px" />
+          </a>
+          <a
+            className="Footer-social-media-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/MyCryptoHQ/MoneroVision"
+          >
+            <i className="nc-icon nc-logo-github size_24px" />
+          </a>
+        </div>
         <p className="Footer-social-titles">MyCrypto</p>
-        <a
-          className="Footer-social-media-link"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://twitter.com/mycrypto"
-        >
-          <i className="nc-icon nc-logo-twitter size_24px" />
-        </a>
-        <a
-          className="Footer-social-media-link"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/MyCryptoHQ"
-        >
-          <i className="nc-icon nc-logo-github size_24px" />
-        </a>
-        <a
-          className="Footer-social-media-link"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://www.facebook.com/mycryptoHQ"
-        >
-          <i className="nc-icon nc-logo-facebook size_24px" />
-        </a>
-        <a
-          className="Footer-social-media-link"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://www.instagram.com/mycryptohq"
-        >
-          <i className="nc-icon nc-logo-instagram size_24px" />
-        </a>
-        <a
-          className="Footer-social-media-link"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://medium.com/@mycrypto"
-        >
-          <i className="nc-icon nc-logo-medium size_24px" />
-        </a>
+        <div className="Footer-social-titles-wrapper">
+          <a
+            className="Footer-social-media-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/mycrypto"
+          >
+            <i className="nc-icon nc-logo-twitter size_24px" />
+          </a>
+          <a
+            className="Footer-social-media-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/MyCryptoHQ"
+          >
+            <i className="nc-icon nc-logo-github size_24px" />
+          </a>
+          <a
+            className="Footer-social-media-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.facebook.com/mycryptoHQ"
+          >
+            <i className="nc-icon nc-logo-facebook size_24px" />
+          </a>
+          <a
+            className="Footer-social-media-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.instagram.com/mycryptohq"
+          >
+            <i className="nc-icon nc-logo-instagram size_24px" />
+          </a>
+          <a
+            className="Footer-social-media-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://medium.com/@mycrypto"
+          >
+            <i className="nc-icon nc-logo-medium size_24px" />
+          </a>
+        </div>
       </div>
-    </section>
+    </div>
   </footer>
 );
